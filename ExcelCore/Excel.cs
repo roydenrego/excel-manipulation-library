b3ed5a35cc0be3798f9ebf9b741fdb834751aaf8
Royden Rego <roydenrego@softrixz.com>
2019-11-06 19:46:59 +0530

    Add project files.

---
+++ ExcelCore/Excel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+
+namespace ExcelCore {
+
+    public class Excel {
+        private Application app = null;
+        private Workbook currentBook = null;
+        private Worksheet currentSheet = null;
+
+        public Excel() : this(false) {
+
+        }
+
+        public Excel(Boolean isVisible) {
+            app = new Application();
+            app.Visible = isVisible;
+        }
+
+        public void Open(string path) {
+            currentBook = app.Workbooks.Open(path);
+            currentSheet = currentBook.Sheets[1]; // Explicit cast is not required here
+        }
+
+      
+
+        public String GetRows() {
+            int lastRow = currentSheet.Cells.SpecialCells(XlCellType.xlCellTypeLastCell).Row;
+
+            string data = "";
+
+            for (int index = 2; index <= lastRow; index++) {
+                Array myValues = (Array)currentSheet.get_Range("A" +
+                   index.ToString(), "B" + index.ToString()).Cells.Value;
+                data = data + myValues.GetValue(1, 1) + " , " + myValues.GetValue(1, 2) + "\n";
+            }
+            return data;
+        }
+    }
+}
---
+++ ExcelManipulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using ExcelCore;
+using System;
+
+namespace ExcelManipulation {
+    class Program {
+
+       
+
+        static void Main(string[] args) {
+            Console.WriteLine("Starting Program");
+
+            Excel excel = new Excel();
+            excel.Open("C:\\Users\\royde\\Documents\\Workspace\\AutomationAnywhere\\book1.xlsx");
+
+            Console.WriteLine(excel.GetRows());
+        }
+    }
+}
